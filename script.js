fb8671cd8a87e4781e528a56ed77f93e77d2013f
HarshMane2604 <maneharsh72@gmail.com>
2025-01-24 17:17:52 +0530

    Synchronous javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Sync in JS
+// code runs in given sequence
+console.log("One");
+console.log("Two");
+console.log("Three")
+// The output is in sequence
+
+// Asynchronous Programming in JavaScript
  
