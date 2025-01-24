167f2b23545d24efe8326b0cd28f031de6931114
HarshMane2604 <maneharsh72@gmail.com>
2025-01-24 17:37:39 +0530

    Learned about setTimeout function

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ console.log("Two");
 console.log("Three")
 // The output is in sequence
 
+setTimeout(() => {
+  console.log("hello")
+}, 4000);//AFTER 4SEC THE HELLO WILL BE VISIBLE 
+
 // Asynchronous Programming in JavaScript
  
