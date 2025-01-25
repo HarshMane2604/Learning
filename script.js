4e7aa743c03d85921cf390c93f73a1d97b9d0141
HarshMane2604 <maneharsh72@gmail.com>
2025-01-25 14:10:57 +0530

    LEarned about call back hell

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-// Sync in JS
-// code runs in given sequence
-console.log("One");
-console.log("Two");
-console.log("Three")
-// The output is in sequence
+// // Sync in JS
+// // code runs in given sequence
+// //console.log("One");
+// //console.log("Two");
 
-setTimeout(() => {
-  console.log("hello")
-}, 4000);//AFTER 4SEC THE HELLO WILL BE VISIBLE 
+// // The output is in sequence
 
-// Asynchronous Programming in JavaScript
- 
+// setTimeout(() => { // requires call  back
+//   //console.log("hello")
+// }, 4000);//AFTER 4SEC THE HELLO WILL BE VISIBLE 
+// //console.log("Three")
+// //console.log("Four")
+
+// function getData(val, getNextData) {
+//   setTimeout(() => {
+//     console.log(val);
+//     if(getNextData){
+//       getNextData();
+//     }
+//   }, 2000);
+// }
+
+// getData(1, () => {
+//   getData(2, () => {
+//     getData(3, () => {
+//       getData(4, () => {
+//         getData(5)
+//       })
+//     })
+//   })
+// });
+
+// // // Asynchronous Programming in JavaScript
+// // Asynchronous JavaScript refers to the execution 
+// // of code in a non-blocking manner, allowing other
+// // operations to continue while waiting for a task to
+// // complete. This is particularly useful for tasks that
+// // take time, such as network requests, file reading, or
+// // timers.
